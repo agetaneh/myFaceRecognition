4d5255c6764b3ca16025114580bf1bca438e337e
Bao Hoang <38933533+bghoang@users.noreply.github.com>
2019-11-04 17:14:09 -0800

    Update process_realtime.py

--- project_2/process_realtime.py
+++ project_2/process_realtime.py
@@ -69,5 +69,14 @@ for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=
 #def detectAndDisplay(frame, w, h):
     faces = face_cascade.detectMultiScale(frame)
     for (x,y,w,h) in faces:
-	center = (x
+	center = (x + w//2, y + h//2)
+        frame = cv.ellipse(frame, center, (w//2, h//2), 0, 0, 360, (255, 0, 255), 4)
+        faceROI = frame_gray[y:y+h,x:x+w]
+        #-- In each face, detect eyes
+        eyes = eyes_cascade.detectMultiScale(faceROI)
+        for (x2,y2,w2,h2) in eyes:
+            eye_center = (x + x2 + w2//2, y + y2 + h2//2)
+            radius = int(round((w2 + h2)*0.25))
+            frame = cv.circle(frame, eye_center, radius, (255, 0, 0 ), 4)
+    cv.imshow('Capture - Face detection', frame)
     
